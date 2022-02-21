97f459c830fdec67aaa0c12296b034b16456d192
Ruhshan <ruhshan.ahmed@gmail.com>
2022-02-21 12:09:08 +0600

    Editor inializes with syntax highlighting

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <editor></editor>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Editor from './components/Editor.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Editor },
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ app/src/components/Editor.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="editor" style="width: 500px; height: 500px"></div>
+</template>
+
+<script>
+
+import loader from "@monaco-editor/loader";
+
+export default {
+  name: "Editor",
+  async mounted() {
+
+    loader.init().then((monaco) => {
+      
+      const editorOptions = {
+        language: "python",
+        minimap: { enabled: false },
+      };
+      
+      monaco.editor.create(document.getElementById("editor"), editorOptions);
+      
+
+    });
+    
+  },
+};
+</script>
+
+
